b4a2f8383aa76ef67144b6e51de548876defe8d0
thiagobcf <thiagobcfonseca@gmail.com>
2023-10-31 13:03:52 -0300

    Fluent API

--- ApiCatalogoPJ/Context/AppDbContext.cs
+++ ApiCatalogoPJ/Context/AppDbContext.cs
@@ -16,5 +16,24 @@ namespace ApiCatalogoPJ.Context
         
         public DbSet<Produto>? Produtos { get; set; }
         public DbSet<Categoria>? Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {   
+            // categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+            // produto
+            mb.Entity<Produto>().HasKey(c => c.ProdutoId);
+            mb.Entity<Produto>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(c => c.Descricao).HasMaxLength(150);
+            mb.Entity<Produto>().Property(c => c.Imagem).HasMaxLength(100);
+            mb.Entity<Produto>().Property(c => c.Preco).HasPrecision(14, 2);
+
+            // relacionamento
+            mb.Entity<Produto>().HasOne<Categoria>(c => c.Categoria).WithMany(p => p.Produtos).HasForeignKey(c => c.CategoriaId);
+
+        }
     }
 }
