c2789cc5b2bbe3f0ae0983f797bd50ab3f0a65b1
thiagobcf <thiagobcfonseca@gmail.com>
2023-11-06 12:09:50 -0300

    add endpoints Produtos

--- ApiCatalogoPJ/Models/Produto.cs
+++ ApiCatalogoPJ/Models/Produto.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ApiCatalogoPJ.Models
@@ -15,6 +16,7 @@ namespace ApiCatalogoPJ.Models
         public DateTime DataCompra { get; set; }
         public int Estoque { get; set; }
         public int CategoriaId { get; set; }
+        [JsonIgnore]
         public Categoria? Categoria { get; set; }
 
     }
