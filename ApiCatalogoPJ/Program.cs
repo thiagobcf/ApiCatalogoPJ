7a9cff86c4a0c5376f146ce5513f9a592646e9cf
thiagobcf <thiagobcfonseca@gmail.com>
2023-11-08 11:43:51 -0300

    restructuring final

--- ApiCatalogoPJ/Program.cs
+++ ApiCatalogoPJ/Program.cs
@@ -13,72 +13,13 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.//ConfigureServices
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogo", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = @"JWT Authorization header using the Bearer scheme.
-                    Enter 'Bearer'[space].Example: \'Bearer 12345abcdef\'",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                          new OpenApiSecurityScheme
-                          {
-                              Reference = new OpenApiReference
-                              {
-                                  Type = ReferenceType.SecurityScheme,
-                                  Id = "Bearer"
-                              }
-                          },
-                         new string[] {}
-                    }
-                });
-});
-
-
-var connectionString = builder.Configuration
-                       .GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-                 options
-                 .UseMySql(connectionString, 
-                 ServerVersion.AutoDetect(connectionString)));
-
-builder.Services.AddSingleton<ITokenService>(new TokenService());
-
-builder.Services.AddAuthentication
-                 (JwtBearerDefaults.AuthenticationScheme)
-                 .AddJwtBearer(options =>
-                 {
-                     options.TokenValidationParameters = new TokenValidationParameters
-                     {
-                         ValidateIssuer = true,
-                         ValidateAudience = true,
-                         ValidateLifetime = true,
-                         ValidateIssuerSigningKey = true,
-
-                         ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                         ValidAudience = builder.Configuration["Jwt:Audience"],
-                         IssuerSigningKey = new SymmetricSecurityKey
-                         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-                     };
-                 });
-
-builder.Services.AddAuthorization();
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAutenticationJwt();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.//Configure
 
 app.MapAutenticacaoEndpoints();
 app.MapCategoriasEndpoints();
